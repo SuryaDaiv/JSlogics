080d69f5ba0e0e5b90bbfd1b3ee1e32ea81a8039
SuryaDaiv <37395409+SuryaDaiv@users.noreply.github.com>
2018-06-23 02:10:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var driverA = document.getElementById("driverA");
+
+var tripA = document.getElementById("formA");
+
+function myFunction() 
+{
+
+    var x = document.getElementById("formA");
+    var text = x.elements[1].value;
+    var Atrip = text.split(" ");  
+    
+    if (driverA=Atrip[1]) {
+        var driverAmiles = Atrip[2];
+        document.getElementById("demo").innerHTML = driverAmiles; 
+    }
+}
+
+
+
+    for (let i = 1; i < 2; i++) {
+        if (driverA=Atrip[i]) {
+
+        for (let k = 0; k < array.length; k++) {
+            const element = array[k];
+            
+        }
+    }
+
+
+*/
+
+
+
+
+/*
+function myFunction() {
+    var x = document.getElementById("formA");
+    var text = x.elements[5].value;
+    var Atrip = text.split(" "); 
+
+var Btrip = tripA.split(" ");
+var Btrip = tripA.split(" ");
+*/
